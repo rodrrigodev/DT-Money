3578ec444499a9aaffd1fcc7e997e9a74131df49
rodrrigodev <rodrigosanyy@gmail.com>
2023-01-18 10:44:14 -0300

    check btn

--- src/components/NewTransationModal/index.tsx
+++ src/components/NewTransationModal/index.tsx
@@ -21,12 +21,12 @@ export function NewTranstionModal() {
 
                     <TransactionType>
 
-                        <TransactionTypeButton variant='income'>
+                        <TransactionTypeButton variant='income' value='income'>
                             <ArrowCircleUp size={24}/>
                             Entrada
                         </TransactionTypeButton>
 
-                        <TransactionTypeButton variant='outcome'>
+                        <TransactionTypeButton variant='outcome' value='outcome'>
                         <ArrowCircleDown size={24}/>
                             Saída
                         </TransactionTypeButton>
