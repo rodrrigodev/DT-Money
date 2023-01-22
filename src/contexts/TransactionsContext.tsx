38c3d6343b92c966fba1714eb0fdbdf0f3060612
rodrrigodev <rodrigosanyy@gmail.com>
2023-01-22 20:42:35 -0300

    search input

--- src/contexts/TransactionsContext.tsx
+++ src/contexts/TransactionsContext.tsx
@@ -10,7 +10,8 @@ interface Transaction {
 }
 
 interface TransactionsContextType {
-    transactions: Transaction[]
+    transactions: Transaction[],
+    fetchTransactions: (query?: string) => Promise<void>
 }
 
 interface TransactionsProviderProps {
@@ -19,23 +20,26 @@ interface TransactionsProviderProps {
 
 export const TransactionsContext = createContext({} as TransactionsContextType)
 
-
-
-
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
     const [transactions, setTransactions] = useState<Transaction[]>([])
-    async function loadTransactions() {
 
-        const response = await fetch("http://localhost:3000/transactions")
+    async function fetchTransactions(query?: string) {
+        const url = new URL("http://localhost:3000/transactions")
+
+        if(query){
+            url.searchParams.append("q", query)
+        }
+
+        const response = await fetch(url)
         const data = await response.json()
         setTransactions(data)
     }
 
     useEffect(() => {
-        loadTransactions()
+        fetchTransactions()
     }, [])
     return (
-        <TransactionsContext.Provider value={{ transactions }}>
+        <TransactionsContext.Provider value={{ transactions, fetchTransactions }}>
             {children}
         </TransactionsContext.Provider>
     )
